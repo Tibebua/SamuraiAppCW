fda28ddf07a3cfc9df9d1ea8afce41017d5f1905
Alazar <tibebualazar@gmail.com>
2021-08-12 20:45:57 +0300

    get by city(owned property in samurai) added

---
+++ SamuraiAppCW/Data/Migrations/20210812160055_FormalNameOwnedPropertyAddedToSamurai.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiAppCW.Data.Migrations
+{
+    public partial class FormalNameOwnedPropertyAddedToSamurai : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Samurais",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Samurais",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Samurais");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Samurais");
+        }
+    }
+}
